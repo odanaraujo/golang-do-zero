95e48b46435992d2db6b252e992e212e730b22d0
odanaraujo <D@nS@142720>
2022-08-25 16:04:28 -0300

    arrays internos - make

--- main.go
+++ main.go
@@ -1,42 +1,25 @@
 package main
 
-import (
-	"fmt"
-	"reflect"
-)
+import "fmt"
 
 func main() {
 
-	var array1 [5]int8
-	array1[0] = 1
-	fmt.Println(array1)
-
-	array2 := [2]string{"Dan", "Araújo"}
-	fmt.Println(array2)
-
 	/*
-		Uma forma de deixar mais dinâmico, onde os três pontinhos,
-		vão fixar o tamanho desse array, de acordo com a quantidade de valores informados
+		make é uma função que serve para criar uma fatia
+		ele recebe três parâmetros: 1 - tipo do slice 2 - o tamanho do slice 3 - Capacidade máxima do slice
+		caso passe da capacidade, ele dobra a capacidade inicial. No exemplo abaixo, começa no 8 e pula para 16
 	*/
-	array3 := [...]int{1, 2, 3, 4, 5, 6}
-	fmt.Println(array3)
 
-	// SLICE
+	slice := make([]uint16, 2, 4)
 
-	slice := []string{"Dan", "Sabrina"}
+	slice = append(slice, 10, 20, 30, 50, 100, 200, 300)
+
+	fmt.Println(len(slice))
+	fmt.Println(cap(slice))
 	fmt.Println(slice)
 
-	slice = append(slice, "Maju")
-	fmt.Println(slice)
+	slice2 := make([]uint16, 0)
 
-	//slice não é uma array
-	fmt.Println(reflect.TypeOf(array1))
-	fmt.Println(reflect.TypeOf(slice))
-
-	slice2 := array3[1:3] //1 é inclusivo e 3 é exclusivo
-	fmt.Println(slice2)
-
-	array3[1] = 4
-
+	slice2 = append(slice2, 87, 90, 8)
 	fmt.Println(slice2)
 }
