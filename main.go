af993d916a2acf0cf2e914db099da6159d5322d5
odanaraujo <D@nS@142720>
2022-08-25 17:55:12 -0300

    estrutura de controle

--- main.go
+++ main.go
@@ -4,41 +4,17 @@ import "fmt"
 
 func main() {
 
-	//chave e valor tem que ser do mesmo tipo
-	usuario := map[string]string{
-		"nome":      "Dan",
-		"sobreNome": "Araújo",
-		"idade":     "32",
+	numero := 10
+
+	//if init - a variável outroNumero tá limitada ao escopo do if
+
+	if outroNumero := numero; outroNumero > 5 {
+		fmt.Println("Número é maior mesmo")
+	} else {
+		fmt.Println("Número náo é maior, não.")
 	}
 
-	fmt.Println(usuario)
-	fmt.Println(usuario["nome"], usuario["sobreNome"], "Tem", usuario["idade"], "anos")
-
-	//map aninhado
-
-	usuario2 := map[string]map[string]string{
-		"dadosPessoais": {
-			"nome":      "Sabrina",
-			"sobreNome": "Lopes",
-			"idade":     "33",
-		},
-		"endereco": {
-			"rua": "Rua do golang",
-			"cep": "09494030",
-		},
+	if numero > 3 {
+		fmt.Println("deu certo")
 	}
-
-	fmt.Println(usuario2["endereco"]["rua"])
-
-	//deletando
-	delete(usuario2, "endereco")
-	fmt.Println(usuario2)
-
-	//adicionando um novo map
-	usuario2["cursos"] = map[string]string{
-		"Sistema da informação": "completo",
-		"Enfermagem":            "Cursando",
-	}
-
-	fmt.Println(usuario2)
 }
