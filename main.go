69b0f07f774408ecb67a4a5b49f7fc4957e32112
odanaraujo <D@nS@142720>
2022-08-26 18:33:56 -0300

    função variática

--- main.go
+++ main.go
@@ -2,14 +2,24 @@ package main
 
 import "fmt"
 
-func calculosMatematicos(valor1, valor2 int) (soma int, subtracao int) {
-	soma = valor1 + valor2
-	subtracao = valor1 - valor2
-	return
+func soma(numeros ...int) {
+
+	total := 0
+	for _, numero := range numeros {
+		total += numero
+	}
+	fmt.Println(total)
+}
+
+func textoComNumeros(palavra string, numeros ...int) {
+
+	for _, numero := range numeros {
+		fmt.Println(palavra, numero)
+	}
 }
 
 func main() {
 
-	fmt.Println(calculosMatematicos(4, 2))
-
+	soma(4, 2, 3, 5, 6)
+	textoComNumeros("Dan", 87, 90, 8)
 }
