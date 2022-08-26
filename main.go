03dabc82efb294383fae2d1963a56dcbff6776c8
odanaraujo <D@nS@142720>
2022-08-26 18:10:59 -0300

    função com retorno nomeado

--- main.go
+++ main.go
@@ -1,53 +1,15 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "fmt"
+
+func calculosMatematicos(valor1, valor2 int) (soma int, subtracao int) {
+	soma = valor1 + valor2
+	subtracao = valor1 - valor2
+	return
+}
 
 func main() {
-	i := 0
 
-	//opção de for
-	for i < 10 {
-		i++
-		time.Sleep(2 * time.Second)
-		fmt.Println("Incrementando i")
-	}
+	fmt.Println(calculosMatematicos(4, 2))
 
-	//for padrão
-	for j := 10; j > 0; j-- {
-		time.Sleep(2 * time.Second)
-		fmt.Println("Decrementando J")
-	}
-
-	//for em uma array
-
-	nomes := [3]string{"Dan", "Sabrina", "Maju"}
-
-	for indice, nome := range nomes {
-		time.Sleep(time.Second)
-		fmt.Println(indice, nome)
-	}
-
-	//for em uma string
-	for indice, letra := range "PALAVRA" {
-		time.Sleep(time.Second)
-		fmt.Println(indice, string(letra)) // colocar string(letra) caso contrário, traz o código da tabela asc
-	}
-
-	//for em um map
-
-	usuario := map[string]string{
-		"nome":      "Dan",
-		"sobreNome": "Araújo",
-		"idade":     "32",
-		"altura":    "184",
-	}
-
-	slice := make([]string, 0)
-	for _, infos := range usuario {
-		slice = append(slice, infos)
-	}
-	fmt.Println(slice[0])
 }
