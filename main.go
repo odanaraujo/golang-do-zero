b177b7353ccafd25a00ae5df04bd5dd242697964
odanaraujo <D@nS@142720>
2022-08-29 23:59:39 -0300

    função  anônima

--- main.go
+++ main.go
@@ -2,24 +2,19 @@ package main
 
 import "fmt"
 
-func soma(numeros ...int) {
-
-	total := 0
-	for _, numero := range numeros {
-		total += numero
-	}
-	fmt.Println(total)
-}
-
-func textoComNumeros(palavra string, numeros ...int) {
-
-	for _, numero := range numeros {
-		fmt.Println(palavra, numero)
-	}
-}
-
 func main() {
 
-	soma(4, 2, 3, 5, 6)
-	textoComNumeros("Dan", 87, 90, 8)
+	/**
+	função que não tem nome
+	*/
+
+	func(texto string) {
+		fmt.Println(texto)
+	}("Dan")
+
+	retorno := func(texto string, numero int) string {
+		return fmt.Sprintf("Recebido -> %s %d", texto, numero)
+	}("Passando parâmetro", 10)
+
+	fmt.Println(retorno)
 }
