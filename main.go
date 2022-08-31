a81f061c8bc371d2b413f58eb0e2176dba865ceb
= <=>
2022-08-31 01:02:09 -0300

    funcao recursiva - refactor

--- main.go
+++ main.go
@@ -6,9 +6,7 @@ func fibonacci(posicao uint) uint {
 	if posicao <= 1 {
 		return posicao
 	}
-	teste := fibonacci(posicao-2) + fibonacci(posicao-1)
-	fmt.Println(teste)
-	return teste
+	return fibonacci(posicao-2) + fibonacci(posicao-1)
 }
 
 func main() {
