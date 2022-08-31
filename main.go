0ca547c6eb5dcff30cc648ec82de03339b84f3f3
odanaraujo <D@nS@142720>
2022-08-31 17:39:34 -0300

    metodo

--- main.go
+++ main.go
@@ -1,20 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-var n int
+type usuario struct {
+	nome  string
+	idade uint8
+}
 
-func init() {
-	fmt.Println("antes do main")
-	n = 10
+func (u usuario) salvar() {
+	fmt.Printf("Salvando os dados do usuário %s no banco de dados\n", u.nome)
+	time.Sleep(time.Second * 3)
+	fmt.Println("Salvo com sucesso!")
+}
+
+func (u usuario) update() {
+	fmt.Printf("Atualizando os dados do usuário %s no banco de dados \n", u.nome)
+	time.Sleep(time.Second * 4)
+	fmt.Println("Dados atualizados com sucesso")
+}
+
+func (u usuario) maiorDeIdade() bool {
+	return u.idade > 18
+}
+
+func (u *usuario) felizAniversario() {
+	u.idade++
 }
 
 func main() {
+
 	/*
-		Mesmo não chamando dentro da main, ela é chamada.
-		podemos ter um init por arquivo e não por pacote.
-		Podemos iniciar uma variável dentro da init
+		Tecnicamente, método não é igual a uma função
+		ela pode tá associada a uma struct, interface, etc.
 	*/
-	fmt.Println(n)
-	fmt.Println("Dentrod o main")
+
+	usuario := usuario{"Dan", 32}
+	fmt.Println(usuario)
+	usuario.salvar()
+	usuario.update()
+	fmt.Println(usuario.maiorDeIdade())
+	usuario.felizAniversario()
+	fmt.Println(usuario.idade)
 }
