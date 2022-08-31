81efd5e5ff60e5aa5efc20f7aaf998c5617f1274
odanaraujo <D@nS@142720>
2022-08-31 17:17:26 -0300

    funçã init

--- main.go
+++ main.go
@@ -2,14 +2,19 @@ package main
 
 import "fmt"
 
+var n int
+
 func init() {
 	fmt.Println("antes do main")
+	n = 10
 }
 
 func main() {
 	/*
 		Mesmo não chamando dentro da main, ela é chamada.
 		podemos ter um init por arquivo e não por pacote.
+		Podemos iniciar uma variável dentro da init
 	*/
+	fmt.Println(n)
 	fmt.Println("Dentrod o main")
 }
