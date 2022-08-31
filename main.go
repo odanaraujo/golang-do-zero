f3d31fc09af1085876a85bd8b19dc38e798906cd
odanaraujo <D@nS@142720>
2022-08-31 17:03:14 -0300

    funcao closure

--- main.go
+++ main.go
@@ -2,31 +2,27 @@ package main
 
 import "fmt"
 
-func recuperarExecucao() {
-	if r := recover(); r != nil {
-		fmt.Println("Execução recuperada com sucesso")
-	}
-}
+func closure() func() {
+	texto := "Dentro da função closure"
 
-func calcularNota(notaPrimeiroSemestre, notaSegundoSemestre float32) bool {
-
-	defer recuperarExecucao()
-
-	media := (notaPrimeiroSemestre + notaSegundoSemestre) / 2
-
-	if media >= 7 {
-		return true
+	funcao := func() {
+		fmt.Println(texto)
 	}
 
-	if media < 6 {
-		return false
-	}
-	panic("A média não pode ser igual a 6")
-
+	return funcao
 }
 
 func main() {
 
-	fmt.Println(calcularNota(6, 6))
-	fmt.Println("Pós execução da função")
+	/**
+	funções que referenciam variáveis que estão fora do seu corpo
+	*/
+
+	texto := "Dentro da função main"
+	fmt.Println(texto)
+
+	funcaoNova := closure() //variavel do tipo funcao
+
+	funcaoNova() //a referência vai ser dentro da função closure(texto dentro dela será impresso)
+
 }
