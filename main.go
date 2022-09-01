82eb072866b5818b838f0bf89b18c87559d1affe
= <=>
2022-08-31 23:21:01 -0300

    wait-group

--- main.go
+++ main.go
@@ -2,28 +2,37 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
 func main() {
 
 	/*
-		Existe uma diferência entre concorrência e paralelismo
-		Paralelismo acontece quando duas ou mais tarefas são executadas ao mesmo tempo
-		Já na concorrência, não necessariamente estão executando ao mesmo tempo.
-		Caso tenha um processador com apenas um núcleo, ele também executa.
-		Nesse caso, ele estaria revezando a execução
-	*/
 
-	go escrever("Dan") //executa a linha 18 mas não espera ela finalizar para seguir o programa
-	escrever("Sabrina")
+	 */
 
+	var waitGroup sync.WaitGroup
+
+	waitGroup.Add(2) //adicionando duas goroutines que ele precisa esperar terminar
+
+	//primeira goroutine
+	go func() {
+		escrever("Dan")
+		waitGroup.Done() // -1 quando a função finaliza, ele termina e tira 1 goroutine do count 2.
+	}()
+
+	go func() {
+		escrever("Sabrina")
+		waitGroup.Done() // -1
+	}()
+
+	waitGroup.Wait() //fala para o programa para ele aguardar o waitgroup chegar em zero
 }
 
 func escrever(texto string) {
-	for {
-		fmt.Println("Olá, ", texto)
-
-		time.Sleep(time.Second * 2)
+	for i := 0; i < 5; i++ {
+		fmt.Println(texto)
+		time.Sleep(time.Second)
 	}
 }
