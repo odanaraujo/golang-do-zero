e27628c982de30b7cba24509194010ee787d571c
= <=>
2023-02-14 23:59:52 -0300

    trabalhando com map

--- main.go
+++ main.go
@@ -23,4 +23,16 @@ func main() {
 	}
 	fmt.Println(bytes.NewBuffer(resultJSON))
 
+	c2 := map[string]string{
+		"nome": "Amora",
+		"raca": "Poodler",
+	}
+
+	cachorro2EmJSON, err := json.Marshal(c2)
+
+	if err != nil {
+		log.Fatal("Ocorreu um erro na linha 34")
+	}
+
+	fmt.Println(bytes.NewBuffer(cachorro2EmJSON))
 }
